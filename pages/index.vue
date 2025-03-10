b76c935d30d345269ae85e46cb70b7781a743553
enrico <81193698+enricoopezzo@users.noreply.github.com>
2025-03-10 23:12:51 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="hero min-h-screen">
+        <div class="hero-content flex-col lg:flex-row">
+            <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                class="max-w-sm rounded-lg shadow-2xl" />
+            <div>
+                <h1 class="text-5xl font-bold">Box Office News!</h1>
+                <p class="py-6">
+                    Il compito del Cappello Parlante è assai arduo e ricco di responsabilità. Rispondi sinceramente alle
+                    prossime domande senza cercare di forzarne la scelta e scopri in quale delle quattro Case di
+                    Hogwarts sarai smistato.
+                </p>
+                <button class="btn btn-primary">Iniziamo</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
